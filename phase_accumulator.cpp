b54a0d5c13cf83307340d15b7c7b259168b5a3cd
arthurkowskii <arthur.cm@icloud.com>
2026-07-18 00:29:13 +0200

    calculated phaseIncrement before the loop for efficiency

--- phase_accumulator.cpp
+++ phase_accumulator.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <fstream>
 
-
 int main(){
-    float frequency = 2;
-    float samplerate = 10;
+    const float frequency = 2;
+    const int samplerate = 10;
+    const float phaseIncrement = frequency / samplerate;
     float phase = 0.0f;
 
     for (int i = 0; i < samplerate; ++i){
         std::cout << "phase is " << phase << " & index is " << i << " \n";
-        phase += frequency / samplerate;
-
+        phase += phaseIncrement;
         if (phase >= 1){
          phase -= 1.0f;
         }
-
     }
-
     return 0;
 }
