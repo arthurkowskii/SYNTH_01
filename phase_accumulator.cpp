acac4d0c82640d0bd638732ae9439ee13f7a82fb
arthurkowskii <arthur.cm@icloud.com>
2026-07-19 14:19:58 +0200

    phase calculation in phase_accumulator.cpp

--- phase_accumulator.cpp
+++ phase_accumulator.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
+
+
+
 
 int main(){
     const float frequency = 2;
     const int samplerate = 10;
     const float phaseIncrement = frequency / samplerate;
+    const float Pi = 3.14159265358979323846f;
+    float sample;
     float phase = 0.0f;
 
     for (int i = 0; i < samplerate; ++i){
-        std::cout << "phase is " << phase << " & index is " << i << " \n";
+        sample = sin(2 * Pi * phase);
+        std::cout << "phase is " << phase << " & index is " << i << "sample is " << sample << " \n";
         phase += phaseIncrement;
         if (phase >= 1){
          phase -= 1.0f;
